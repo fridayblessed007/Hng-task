0b71c3b1e03b8a51698475558ed322580c3ac759
Chibuzo <uchibuzo1@gmail.com>
2022-11-02 00:14:01 +0100

    first commit

---
+++ Zuri.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Style.css';
+const Zuri=()=>{
+    return(
+        <div>
+    
+    {/* <img src="BLESSED007.jpg" id="profile__img" alt> */}
+    <div class="profile-name">Friday Blessed</div>
+    <a href="https://twitter.com/FridayBlessed13" class="links">Twitter Link</a>
+    <a href="https://training.zuri.team/" class="links">Zuri Team</a>
+    <a href="https://books.zuri.team" class="links">Zuri Books
+    </a>
+    <a href="https://books.zuri.team" class="links">Python Book
+    </a>
+    <a href="background.zuri.team" class="links">Background Checks for Codes</a>
+    <a href="https://books.zuri.team/design-rules" class="links">Design Books</a>
+    <div class="container"> 
+        <div> <i class="fa-brands fa-github "></i>
+         <i class="fa-brands fa-slack"></i>
+        </div>
+     
+    </div>
+    <div class="bottom-text">Zuri<span style="color: red;" > .</span>Internship</div>
+    
+
+    <div class="footer">HNG Internship 9 Frontend Task</div>
+    <div>
+        {/* <img class="bottom-pic" src="About-Ingressive-For-Good.png"> */}
+    </div>
+        </div>
+    )
+}
+
+export default Zuri;
